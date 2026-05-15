ed05591e23d07aefad2b9cbcf32a4889666792c7
Aleksandr Potekhin <55990036+rendleks@users.noreply.github.com>
2026-05-15 15:47:23 +0300

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -4,22 +4,22 @@ from datetime import datetime
 from pathlib import Path
 
 from aiogram import Bot, Dispatcher
-from aiogram import types
+from aiogram.types import Message
 
 from app import TOKEN
 
 dp = Dispatcher()
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+@dp.message(Command('start', 'help'))
+async def send_welcome(message: Message):
     await message.reply(
         f'Я бот. Приятно познакомиться, {message.from_user.first_name}'
     )
 
 
-@dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def text_reply(message: types.Message):
-    if message.text.lower() == 'привет':
+@dp.message()
+async def text_reply(message: Message):
+    if message.text and message.text.lower() == 'привет':
         await message.answer('Привет!')
     else:
         await message.answer('Не понимаю, что это значит...')
