67df0b2b4946d72c01d50d0288752e7ea4f31bcb
Aleksandr Potekhin <55990036+rendleks@users.noreply.github.com>
2026-05-15 15:26:30 +0300

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -11,6 +11,19 @@ from app import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply(
+        f'Я бот. Приятно познакомиться, {message.from_user.first_name}'
+    )
+
+
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def text_reply(message: types.Message):
+    if message.text.lower() == 'привет':
+        await message.answer('Привет!')
+    else:
+        await message.answer('Не понимаю, что это значит...')
 
 
 async def main():
